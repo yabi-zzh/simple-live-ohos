e7b2f13f2498097034909a728ac23f9a9b723896
yabi-zzh <ljhjukl@gmail.com>
2026-02-24 11:40:32 +0800

    chore: 升级版本至 v1.0.2 并更新 README

--- lib/app/constants/app_constants.dart
+++ lib/app/constants/app_constants.dart
@@ -3,5 +3,5 @@ class AppConstants {
   AppConstants._();
 
   /// 应用版本号（升级时同步修改 pubspec.yaml 和 ohos/AppScope/app.json5）
-  static const String version = '1.0.0';
+  static const String version = '1.0.2';
 }
